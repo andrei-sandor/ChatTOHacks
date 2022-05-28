1f12c1ad7a9891bec49a3b3edd477d93a405ab23
Deep3213 <106452223+Deep3213@users.noreply.github.com>
2022-05-28 19:58:17 -0400

    Add files via upload

---
+++ emojiTranslator.py
@@ -0,0 +1,25 @@
+# Program that translates text from MP3 File into emojis
+
+text = input("Please enter a message: ")
+
+def emojiMaker():
+
+    text.lower()
+    if "happy" in text:
+        print(text + " \U0001f601")      # Method 1 of displaying emoji (through code)
+    elif "sad" in text:
+        print(text + " 😢")              # Method 2 of displaying emoji (through directly putting the emoji in
+    elif "angry" in text:
+        print(text + " 😡")
+    elif "scared" in text:
+        print(text + " 😱")
+    elif "worried" in text:
+        print(text + " 😳")
+    elif "cool" in text:
+        print(text + " 😎")
+    elif "smart" in text:
+        print(text + " 🧠")
+    else:
+        print(text)
+
+emojiMaker()
