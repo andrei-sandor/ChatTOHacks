4d690acf917ee05007a2188fc49bc7754f44325a
andrei-sandor <97865484+andrei-sandor@users.noreply.github.com>
2022-05-29 11:07:05 -0400

    Update emojiTranslator.py

--- emojiTranslator.py
+++ emojiTranslator.py
@@ -1,11 +1,9 @@
 # Program that translates text from MP3 File into emojis
 
-text = input("Please enter a message: ")
-
-def emojiMaker():
+def emojiMaker(text):
 
     text.lower()
-<<<<<<< HEAD
+
     emoji = ""
     if "happy" in text:
         emoji += " \U0001f601"      # Method 1 of displaying emoji (through code)
@@ -23,26 +21,6 @@ def emojiMaker():
         emoji +=  + " 🧠"
 
     print(text + emoji)
-=======
-    i = 0
 
-    while (i < len(text)):
-        i = i + 1
 
-    if "happy" in text:
-            print(text + " 😄")
-    if "sad" in text:
-        print(text + " 😢")
-    if "angry" in text:
-        print(text + " 😡")
-    if "scared" in text:
-        print(text + " 😱")
-    if "worried" in text:
-        print(text + " 😳")
-    if "cool" in text:
-        print(text + " 😎")
-    if "smart" in text:
-        print(text + " 🧠")
->>>>>>> 1aee9a41ac60a96dd25984000ad320e4a6da4ec7
 
-emojiMaker()
