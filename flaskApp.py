75eaeac2d6e8763b8b74026cf1f16915b263cea7
andrei-sandor <97865484+andrei-sandor@users.noreply.github.com>
2022-05-28 21:42:20 -0400

    Update flaskApp.py

--- flaskApp.py
+++ flaskApp.py
@@ -6,11 +6,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def sessions():
-<<<<<<< Updated upstream
-    return render_template('ui.html')
-=======
     return render_template('index.html')
->>>>>>> Stashed changes
+
 
 async def textToEmoji(methods = ['GET']):
     await send_receive()
