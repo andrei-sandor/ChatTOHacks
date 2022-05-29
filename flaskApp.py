107df5d736ee803291e706c877330db967ac7769
andrei-sandor <97865484+andrei-sandor@users.noreply.github.com>
2022-05-28 21:38:55 -0400

    Create flaskApp.py
    Started a template for flask

---
+++ flaskApp.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+from ChatMojiBot import *
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def sessions():
+    return render_template('ui.html')
+
+async def textToEmoji(methods = ['GET']):
+    await send_receive()
